804a6b29bc44b986aa5495894c9d1c7472be0f60
황원태 <amlk31255@gmail.com>
2022-11-01 20:00:43 +0900

    Updates

---
+++ _next/static/X00d1__0rVIw4CxxxI8d8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3d084103b59a7ddd.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
