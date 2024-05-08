cc8d1939d8d97677b27aa600483cee4188b9642a
Ferv0r2 <amlk31255@gmail.com>
2024-05-09 01:21:36 +0900

    삭제 누락 부분 수정

--- playlist/src/app/kepler/page.tsx
+++ playlist/src/app/kepler/page.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import { ArticleSection } from '@/components'
 import { MasterLayout } from '@/layout'
 
 export default function Kepler() {
   return (
     <>
       <MasterLayout>
-        <ArticleSection />
+        <div></div>
       </MasterLayout>
     </>
   )
